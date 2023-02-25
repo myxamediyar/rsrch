3cb3cffbe0a299e4d9133478d3f81b09f8ff4fb0
myxamediyar <mukhamediyar.k@gmail.com>
2023-02-24 22:05:33 -0800

    encountered errors in implementation, will fix later

--- omp-stuff.py
+++ omp-stuff.py
@@ -10,6 +10,12 @@ def getAsci(char):
         if char in j:
             char = d[i]
     return ord(char)
+def normalize(word):
+    if type(word) == str:
+        word = toIntLi(toBin(word))
+    norm = np.sqrt(np.dot(word, word))
+    return word / norm
+
 
 def toBin(word):
     res = ""
@@ -33,18 +39,26 @@ def corr(word1, word2):
 
 #run OMP-esque circorr, find "count" closest (upperbound)
 def findClosest(word, text, count):
-    text = text.split()
-    ompRes = []
-    wordAuto = autocorr(word) / len(word)
-    autoDot = np.sqrt(np.dot(wordAuto, wordAuto))
-    for i in text:
-        resCor = corr(word, i) / max(len(i), len(word))
-        resDot = np.sqrt(np.dot(resCor, resCor))
-        ompRes.append((i, resDot))
-    print(ompRes)
-    return (word, autoDot), heapq.nlargest(count, ompRes, key=lambda x: x[1])
+    # text = text.split()
+    # ompRes = []
+    # wordAuto = autocorr(word) / len(word)
+    # autoDot = np.sqrt(np.dot(wordAuto, wordAuto))
+    # for i in text:
+    #     resCor = corr(word, i) / max(len(i), len(word))
+    #     resDot = np.sqrt(np.dot(resCor, resCor))
+    #     ompRes.append((i, resDot))
+    # print(ompRes)
+    # return (word, autoDot), heapq.nlargest(count, ompRes, key=lambda x: x[1])
+    print(1)
 
-text = "dajbefa dafnkja abcdef abdecf àbcdef loool chefCurry"
+text = "dajbefa dafnkja abcdef abdecf àbcdef loool chefCurry LOOOL"
 word = "abcdef"
 
 print(findClosest(word, text, 2))
+
+#Run OMP with word and text, find "count" matches (upperbound)
+def omp(word, text, count):
+    #TO DO: count occurences of letters, run OMP
+    #cuz if our word is [1,0,0,1] and text is [1,0,0,1,0,0,0,1,1,0] if we do
+    #text - word shift 0 we get [0,0,0,0,0,0,0,1,1,0], make sure its max(subtraction, 0)
+    print(1)
